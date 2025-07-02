bbc43678d75ba6819ed860e355ba2a8901a62eb6
WR624KQI <0000877673@senaimgaluno.com.br>
2025-07-02 19:06:49 -0300

    feature: ciracao da pagina inicial

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,11 @@
 export default function Home() {
   return (
     <>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-200 gap 20px">
+        <h1 className="text-4xl font-bold mb-4 text-gray-800">Prova de Versionamento - RPV Senai Cataguases</h1>
+        <p className="text-2xl mb-2 text-slate-500">Heitor Pinheiro Tavares</p>
+        <p className="text-2xl text-slate-400">Cataguases, 02 de Julho de 2025</p>
+      </div> 
     </>
   )
 }
